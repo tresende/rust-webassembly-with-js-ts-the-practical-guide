1edd59446a25ece348bf8cb9259ec7be7bcead65
Thiago Resende <thiago.resende@ifood.com.br>
2022-08-20 17:13:05 -0300

    Mutable variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let message = "Hello World";
+    let mut message = "Hello World";
+    println!("{}", message);
+    message = "Hi there";
     println!("Some Text: {}", message);
+
+    let age = 12;
+    println!("{}", age);
+    let age = 39;
+    println!("{}", age);
 }
 
 
