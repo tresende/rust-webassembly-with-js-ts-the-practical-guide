87abcd57f8504fa6fff99f7990342db394f5a87f
Thiago Resende <thiago.gcresende@gmail.com>
2022-08-24 11:13:34 -0300

    primitive data types

--- main.rs
+++ main.rs
@@ -1,18 +1,11 @@
 fn main() {
-    let is_it_fun = false;
-    // i32 -> signed integer of 32bits
-    // -> signed can hold positive and negative values
-    let num = -10;
+    let custom_num = 98_888;
+    let hex_num = 0xfa;
+    let bin_num = 0b0010_1111;
+    let byte_num = b'A';
 
-    // u32 -> 2^32 - 1
-    // u8 -> unsigned integer of 8bits
-    // 2^8 - 1 = 255
-    let small_num: u8 = 255;
-
-    // -2^7 -> 2^7 - 1
-    // -128 -> 127
-    let small_num_2: i8 = 127;
-
-    let sys_num: isize = -10;
-    let sys_num_2: isize = 10;
+    println!("{}", custom_num);
+    println!("{}", hex_num);
+    println!("{}", bin_num);
+    println!("{}", byte_num);
 }
