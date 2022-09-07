6e1dda13230e5874fe9c9e925b3bc9953b77185a
Thiago Resende <thiago.gcresende@gmail.com>
2022-09-07 17:23:20 -0300

    primitive data types: string

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let float_num: f32 = 3.14;
+    /* let float_num: f32 = 3.14;
     let float_num_2: f64 = 3.2334327489;
 
     let tup: (i32, &str, u8) = (20, "Hello", 1);
@@ -14,5 +14,27 @@ fn main() {
     println!("{}", x[2]);
 
     let y = [2; 6]; // [2, 2, 2, 2, 2, 2]
-    println!("{}", y[5]);
+    println!("{}", y[5]); */
+
+    let a = 10;
+    let b = a;
+    let c = 15;
+    let d = add(a, b);
+
+    println!("A:{}", a);
+    println!("B:{}", b);
+    println!("C:{}", c);
+    println!("D:{}", d);
+
+    let message = String::from("Hello");
+    let message2 = message;
+
+    println!("{}", message);
+    println!("{}", message2);
+}
+
+fn add(x: u32, y: u32) -> u32 {
+    let sum = x + y;
+    //return result;
+    sum
 }
