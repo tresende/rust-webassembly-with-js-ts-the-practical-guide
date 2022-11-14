576c43b7eae5c186a2f49a93cebd2522b6d5024e
Thiago Resende <thiago.gcresende@gmail.com>
2022-11-14 11:23:46 -0300

    [feat] AI Mutable Borrow Example

--- main.rs
+++ main.rs
@@ -1,9 +1,15 @@
 fn main() {
     let mut message = String::from("Hello");
+    let message3 = &message;
+
     let message2 = &mut message; // & -> borrowed reference
 
-    message2.push_str(" world");
-
+    unpredictable_mutate(message2);
     println!("{}", message2);
+
     println!("{}", message);
+}
+
+fn unpredictable_mutate(val: &mut String) {
+    val.push_str("_unpredictable")
 }
