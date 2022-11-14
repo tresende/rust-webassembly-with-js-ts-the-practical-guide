6b699ff0631d5d8b8545cf86f5d287ef3fd0b4df
Thiago Resende <thiago.gcresende@gmail.com>
2022-11-13 21:25:53 -0300

    muttable messages

--- main.rs
+++ main.rs
@@ -1,40 +1,10 @@
 fn main() {
-    /* let float_num: f32 = 3.14;
-    let float_num_2: f64 = 3.2334327489;
-
-    let tup: (i32, &str, u8) = (20, "Hello", 1);
-
-    println!("{}", tup.1);
-
-    let (a, b, c) = tup;
-    println!("{}", a);
-
-    let x = [1, 5, 6, 7];
-
-    println!("{}", x[2]);
-
-    let y = [2; 6]; // [2, 2, 2, 2, 2, 2]
-    println!("{}", y[5]); */
-
-    let a = 10;
-    let b = a;
-    let c = 15;
-    let d = add(a, b);
-
-    println!("A:{}", a);
-    println!("B:{}", b);
-    println!("C:{}", c);
-    println!("D:{}", d);
-
     let message = String::from("Hello");
-    let message2 = message;
-
-    println!("{}", message);
-    println!("{}", message2);
+    let new_message = extend_message(message);
+    println!("new_message: {}", new_message);
 }
 
-fn add(x: u32, y: u32) -> u32 {
-    let sum = x + y;
-    //return result;
-    sum
+fn extend_message(mut a: String) -> String {
+    a.push_str("World");
+    a
 }
