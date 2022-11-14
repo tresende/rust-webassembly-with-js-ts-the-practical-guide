b82da3f8e7d90a2ddc9d33b9775126662752859e
Thiago Resende <thiago.gcresende@gmail.com>
2022-11-13 21:33:02 -0300

    [feat] Copying age

--- main.rs
+++ main.rs
@@ -1,10 +1,20 @@
 fn main() {
     let mut message = String::from("Hello");
     message = extend_message(message);
+
+    let age = 30;
+    extend_age(age);
+    println!("{}", age);
+
     println!("new_message: {}", message);
 }
 
 fn extend_message(mut a: String) -> String {
     a.push_str("World");
+    a
+}
+
+fn extend_age(mut a: u32) -> u32 {
+    a += 100;
     a
 }
