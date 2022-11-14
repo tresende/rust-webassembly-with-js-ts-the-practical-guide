721b954fbdb12533fe4eb4172999d33e7d6d5162
Thiago Resende <thiago.gcresende@gmail.com>
2022-11-14 15:35:31 -0300

    [feat] AI Dereferencing

--- main.rs
+++ main.rs
@@ -1,11 +1,22 @@
 fn main() {
     let a = 10;
     let b = &a;
-    let c = &b;
-    let d = &c;
+    let mut c = &b;
+    let d = b;
 
-    println!("{}", c);
-    println!("{}", a == *b);
-    println!("{}", a == **c);
-    println!("{}", a == ***d);
+    let e = &&100;
+    c = e;
+
+    println!("Value of c: {:p}", c);
+    println!("Value of e: {:p}", e);
+    println!("Address of 100: {:p}", &(**e));
+
+    println!("Value of c: {:p}", *c);
+    println!("Value of e: {:p}", *e);
+    println!("Address of 100: {:p}", &(**c));
+
+    // println!("Address of a: {:p}", &a);
+    // println!("Value of b: {:p}", b);
+    // println!("Value of c: {:p}", c);
+    // println!("Value of d: {:p}", d);
 }
